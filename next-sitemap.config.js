58716a1a40e2becca476b27f1d6ef893200c8eeb
MnD3v <lolo36626835@gmail.com>
2025-07-03 23:20:07 +0000

    sites map

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    siteUrl: 'https://www.evala.tg', // Remplace par l'URL de ton site
+    generateRobotsTxt: true, // Génère automatiquement le fichier robots.txt
+    sitemapSize: 5000, // Limite des URLs par sitemap (optionnel)
+    changefreq: 'daily', // Fréquence de modification des pages
+    priority: 0.7, // Priorité par défaut des pages dans le sitemap
+  };
+  
